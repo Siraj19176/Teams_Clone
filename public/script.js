e2032eeebb8085877fef7d6cc46fcc26c501a5aa
siraj6751 <siraj19176@iiitd.ac.in>
2021-06-30 15:31:15 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,141 @@
+const socket = io("/")
+const grid = document.getElementById('grid');
+
+const myVideo = document.createElement('video')
+
+//const myVideo2=document.createElement('video')
+myVideo.muted = true
+//myVideo2.muted=true
+let myVideoStream;
+var peer = new Peer(undefined, {
+  path: '/p',
+  host: '/',
+  port: '3000'
+})
+
+const peers = {}
+
+const constraints = {
+  audio: true,
+  video: true
+}
+
+const promise = navigator.mediaDevices.getUserMedia(constraints)
+
+promise.then(function(stream) {
+  myVideoStream = stream
+  //addVideoStream(myVideo, stream)
+  //myVideo.srcObject=stream;
+  // myVideo.addEventListener('loadedmetadata', function(){
+  //   myVideo.play();
+  // })
+  // myVideo.play()
+  // grid.append(myVideo)
+  //
+  // myVideo2.srcObject=stream;
+  // myVideo2.play();
+  // grid.append(myVideo2);
+  
+  helper(myVideo, stream)
+  peer.on('call', function(call) {
+    console.log("call aaya ")
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', function(userVideoStream) {
+      console.log("111")
+      helper(video, userVideoStream)
+    })
+
+    call.on('close', function(){
+      console.log("remove video of the peer")
+      video.remove()
+    })
+
+    console.log('peer id ' +peer.id)
+    peers[call.peer]=call
+    console.log(peers)
+
+  })
+
+  socket.on("new-user-in-the-room", (userId) => {
+    console.log("User Connected", userId);
+    //ConnectNewUser(userId, stream);
+    setTimeout(ConnectNewUser, 1000, userId, stream)
+  });
+}).catch(function(err) {
+  console.log("u got an error:" + err)
+})
+
+peer.on('open', function(id) {
+  console.log(id)
+  socket.emit('join-call', ROOM_ID, id)
+})
+
+
+function ConnectNewUser(userId, stream) {
+  var call = peer.call(userId, stream)
+  const video = document.createElement('video')
+  //console.log(stream)
+  call.on('stream', function(userVideoStream) {
+    console.log("222")
+    helper(video, userVideoStream)
+  })
+
+  call.on('close', function(){
+    console.log("remov video inside coonect new user")
+    video.remove()
+  })
+  console.log("we are inside the connect new user function")
+  console.log("peer id "+peer.id)
+  console.log("user id "+userId)
+
+  //peers[peer.id]=call
+  peers[userId]=call
+  console.log(peers)
+
+}
+
+
+function helper(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', function() {
+    video.play();
+  })
+  //video.play();
+  grid.append(video)
+}
+
+function sendMessage() {
+  let msg = document.getElementById('message').value;
+  console.log(msg+" "+msg.length)
+  if (msg.length > 0) {
+    socket.emit("new-message", msg,ROOM_ID);
+  }
+}
+
+socket.on("message", function(message) {
+  console.log(message)
+  var ref = document.getElementById('m-list')
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(message);
+  node.appendChild(textnode);
+  ref.appendChild(node);
+  document.getElementById('message').value='';
+  scroll()
+
+})
+
+
+socket.on('remove-user',function(userId){
+  console.log("remove user with id "+userId)
+  console.log(peers)
+  if(peers[userId]){
+    peers[userId].close();
+  }
+
+
+})
+function scroll(){
+  var ref=document.querySelector('.main__chat__window')
+  ref.scrollTop = ref.scrollHeight;
+}
---
+++ server.js
@@ -0,0 +1,94 @@
+const express=require('express')
+const app = express()
+const http= require("http")
+const server = http.Server(app)
+const {v4: randomId}=require('uuid')
+//const { ExpressPeerServer } = require('peer');
+// import { ExpressPeerServer } from 'peer';
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+const io = require('socket.io')(server)
+const bodyParser=require('body-parser')
+
+let agendas=[]
+app.set("view engine",'ejs')
+
+app.use(express.static('public'))
+
+app.use('/p',peerServer)
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get('/:r',function(req,res){
+  //console.log(agendas)
+  res.render('room',{roomId:req.params.r,newActivity:agendas})
+})
+
+app.get('/', function (req, res){
+  agendas=[]
+  var today=new Date();
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+
+  var date=today.toLocaleDateString("en-Us",options)
+  res.render('agenda',{kindOfDay:date,newActivity:agendas})
+
+})
+
+app.post('/agenda', function (req, res){
+  //console.log(req)
+  agendas.push(req.body.newActivity)
+  //res.redirect('/')
+  var today=new Date();
+  var options = {
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  };
+
+  var date=today.toLocaleDateString("en-Us",options)
+  res.render('agenda',{kindOfDay:date,newActivity:agendas})
+})
+
+app.post('/joinRoom',function(req, res){
+  const tuid=randomId()
+  console.log("id of room="+tuid)
+  res.redirect('/'+tuid)
+})
+
+
+io.on('connection',function(socket){
+  socket.on('join-call',function(room,userId){
+    console.log(userId+" user joined the room "+room)
+    socket.join(room)
+    //console.log(socket.rooms)
+    socket.broadcast.to(room).emit('new-user-in-the-room', userId)
+
+    socket.on('disconnect',function(reason){
+      console.log('user disconnected with id '+userId)
+      socket.broadcast.to(room).emit('remove-user', userId)
+    })
+
+
+  })
+
+  socket.on('new-message',function(msg,ROOM_ID){
+    //console.log(msg)
+    //io.sockets.emit("message",msg)
+    io.to(ROOM_ID).emit("message",msg)
+  })
+
+
+
+
+})
+
+server.listen(process.env.PORT || 3000,function(){
+  console.log("Server Running on 3000")
+})
