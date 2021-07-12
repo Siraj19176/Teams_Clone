98d5e50a8444cbf708c288cf90b9f5fc10e590ad
siraj6751 <siraj19176@iiitd.ac.in>
2021-07-12 11:34:41 +0530

    nickname join

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express=require('express')
+const express = require('express')
 const app = express()
-const http= require("http")
+const http = require("http")
 const server = http.Server(app)
-const {v4: randomId}=require('uuid')
+const {
+  v4: randomId
+} = require('uuid')
 //const { ExpressPeerServer } = require('peer');
 // import { ExpressPeerServer } from 'peer';
 var ExpressPeerServer = require('peer').ExpressPeerServer;
@@ -10,81 +12,104 @@ const peerServer = ExpressPeerServer(server, {
   debug: true
 });
 const io = require('socket.io')(server)
-const bodyParser=require('body-parser')
+const bodyParser = require('body-parser')
 
-let agendas=[]
-app.set("view engine",'ejs')
+let agendas = []
+app.set("view engine", 'ejs')
 
 app.use(express.static('public'))
 
-app.use('/p',peerServer)
+app.use('/p', peerServer)
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 
-app.get('/:r',function(req,res){
+app.get('/:r', function(req, res) {
   //console.log(agendas)
-  res.render('room',{roomId:req.params.r,newActivity:agendas})
+  res.render('room', {
+    roomId: req.params.r,
+    newActivity: agendas
+  })
 })
 
-app.get('/', function (req, res){
-  agendas=[]
-  var today=new Date();
+app.get('/', function(req, res) {
+  agendas = []
+  var today = new Date();
   var options = {
-    weekday:"long",
-    day:"numeric",
-    month:"long"
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   };
 
-  var date=today.toLocaleDateString("en-Us",options)
-  res.render('agenda',{kindOfDay:date,newActivity:agendas})
+  var date = today.toLocaleDateString("en-Us", options)
+  res.render('agenda', {
+    kindOfDay: date,
+    newActivity: agendas
+  })
 
 })
 
-app.post('/agenda', function (req, res){
+app.post('/agenda', function(req, res) {
   //console.log(req)
-  agendas.push(req.body.newActivity)
+  if (req.body.newActivity.length > 0) {
+    agendas.push(req.body.newActivity)
+  }
   //res.redirect('/')
-  var today=new Date();
+  var today = new Date();
   var options = {
-    weekday:"long",
-    day:"numeric",
-    month:"long"
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   };
 
-  var date=today.toLocaleDateString("en-Us",options)
-  res.render('agenda',{kindOfDay:date,newActivity:agendas})
+  var date = today.toLocaleDateString("en-Us", options)
+  res.render('agenda', {
+    kindOfDay: date,
+    newActivity: agendas
+  })
 })
 
-app.post('/joinRoom',function(req, res){
-  const tuid=randomId()
-  console.log("id of room="+tuid)
-  res.redirect('/'+tuid)
+app.post('/joinRoom', function(req, res) {
+  const tuid = randomId()
+  console.log("id of room=" + tuid)
+  res.redirect('/' + tuid)
+})
+app.post('/joinRoomByNickName',function(req, res){
+  //console.log(req.body.nickName)
+  //console.log(req.body.nickName.length)
+  if(req.body.nickName.length>0){
+    const tuid="7017080055"+req.body.nickName+"9557960423"
+    console.log("id of room=" + tuid)
+    res.redirect('/' + tuid)
+  }
+
 })
 
 
-io.on('connection',function(socket){
-  socket.on('join-call',function(room,userId){
-    console.log(userId+" user joined the room "+room)
+io.on('connection', function(socket) {
+  socket.on('join-call', function(room, userId) {
+    console.log(userId + " user joined the room " + room)
     socket.join(room)
     //console.log(socket.rooms)
     socket.broadcast.to(room).emit('new-user-in-the-room', userId)
 
-    socket.on('disconnect',function(reason){
-      console.log('user disconnected with id '+userId)
+    socket.on('disconnect', function(reason) {
+      console.log('user disconnected with id ' + userId)
       socket.broadcast.to(room).emit('remove-user', userId)
     })
-    socket.on('disconnect-me',function(){
+    socket.on('disconnect-me', function() {
       socket.disconnect()
     })
 
 
   })
 
-  socket.on('new-message',function(msg,ROOM_ID){
+  socket.on('new-message', function(msg, ROOM_ID) {
     //console.log(msg)
     //io.sockets.emit("message",msg)
-    io.to(ROOM_ID).emit("message",msg)
+    io.to(ROOM_ID).emit("message", msg)
   })
 
 
@@ -92,6 +117,6 @@ io.on('connection',function(socket){
 
 })
 
-server.listen(process.env.PORT || 3000,function(){
+server.listen(process.env.PORT || 3000, function() {
   console.log("Server Running on 3000")
 })
